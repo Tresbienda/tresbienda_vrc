1545d24e4f7b824ac6ab83d2e2e1bd2333f8f92d
alhafizd <alhafizdhernanda99@gmail.com>
2025-09-19 18:31:35 +0700

    bruh

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import Navbar from "./components/Navbar";
 import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Projects from "./sections/Projects";
-import Skills from "./sections/Skills"; 
 import Contact from "./sections/Contact";
 import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="bg-gray-900 text-white min-h-screen">
+    <div className="bg-gray-900 text-black min-h-screen">
       <Navbar />
       <Hero />
       <About />
